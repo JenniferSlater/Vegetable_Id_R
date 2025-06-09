0810986ac2efb91d7d0dc79e3d1485358f8c545b
Jennifer Slater <jslater5@binghamton.edu>
2025-06-09 12:34:52 -0400

    Update Vegtable identification.R

--- Vegtable identification.R
+++ Vegtable identification.R
@@ -12,9 +12,9 @@ test_filepaths <- dir_ls(test, recurse = TRUE, glob = "*.jpg") #Gets list of all
 
 # "Create a DataFrame with the filepath and the labels of the pictures"
 proc_img <- function(filepaths) {
-  labels <- basename(path_dir(filepaths))
+  labels <- basename(path_dir(filepaths)) #foldernames are labels
   
-  df <- tibble(
+  df <- tibble( #creates a table 
     Filepath = as.character(filepaths),
     Label = labels
   ) %>%
@@ -32,24 +32,25 @@ print(test_df)
 
 # (STEP 2!!! PRINTING DATA SET) EZ!!
 print('-- Training set --')
-print(paste("Number of pictures", nrow(test_df)))
+print(paste("Number of pictures", nrow(test_df))) 
 print(paste("Number of different labels:", length(unique(test_df$Label))))
-print(unique(test_df$Label))
-head(test_df, 5)#works nicely!
+print(unique(test_df$Label)) #prints my list
+head(test_df, 5) #shows only 5 rows
+#works nicely! 
 
-
-#(Step 3!!! Create a DataFrame with one Label of each category)
-
+#(Step 3!!! Create a DataFrame with one Label of each category) lowkey we could cut a lot of this section out and be fine
 #df_unique = train_df.copy().drop_duplicates(subset=["Label"]).reset_index()
-#essentially this would make a copy and drop duplicates
+#essentially this would make a copy and drop duplicates 
+
+#keep one image per label
 unique_df <- test_df %>% 
 # distinct() removes duplicate rows based on the Label column and keep_all = TRUE' keeps all columns in the output
   distinct(Label, .keep_all = TRUE)
 
-# Display some pictures of the dataset
+#(Step 4!!! Display some pictures of the dataset)
 #they want to create a 6*6 column with picture size (8,7)<--Magick can help with that...maybe
 
-# For each image, read it and add the label below
+# For each image, read it and add the label at the bottom
 imgs <- list()
 for (i in 1:nrow(unique_df)) {
   img <- image_read(unique_df$Filepath[i])
@@ -58,15 +59,17 @@ for (i in 1:nrow(unique_df)) {
   imgs[[i]] <- img
 }
 
+# put the images in rows of 6 and stack the rows on top of each other
 rows <- list()
 for (i in seq(1, length(imgs), by = 6)) {
-  # Get up to 6 images safely
+  
+  # Get 6 images 
   row_imgs <- imgs[i:min(i + 5, length(imgs))]
   
-  # Convert list of images to magick-image vector
+  # Convert list of images to magick images 
   row_vec <- do.call(c, row_imgs)
   
-  # Append horizontally (side-by-side)
+  # side by side images
   rows[[length(rows) + 1]] <- image_append(row_vec)
 }
 
